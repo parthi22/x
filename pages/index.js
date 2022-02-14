8d0df721c52a5aa4fd37a706e5803b8f8b8dab94
Parthiban Sudhaman <ext.parthiban.sudhaman@ef.com>
2022-02-14 14:11:18 +0530

    eight

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,8 @@
-import { RequestDemo1, AdvantagesComponent } from '../ui-components/index'
-import CompanyScrool from '../custom-ui-components/CompanyScrool'
-import GoalsHelps from '../custom-ui-components/GoalHelps/GoalsHelps'
+import React from 'react'
+import GoalsAdView from '../components/goals-ad'
 
-export default function Home() {
-  return (
-    <main>
-      <div>
-        <div>
-          <AdvantagesComponent
-            width="100%"
-            overrides={{
-              'Flex.Flex[2].Heading[0]': {
-                height: '100%',
-              },
-              'Flex.Flex[3].Heading[0]': {
-                height: '100%',
-              },
-              'Flex.Flex[4].Heading[0]': {
-                height: '100%',
-              },
-            }}
-          />
-        </div>
-        <div>
-          <RequestDemo1 />
-        </div>
-      </div>
-      <CompanyScrool />
-      <GoalsHelps />
-    </main>
-  )
+const Home = () => {
+  return <GoalsAdView />
 }
+
+export default Home
